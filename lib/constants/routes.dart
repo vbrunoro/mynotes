f54049896cd3f851952df01bb31f8fb1e7a4aa82
vbrunoro <vfmbrunoro@gmail.com>
2023-05-08 11:46:02 -0300

    move to bloc for routing and dialogs

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,7 +1,1 @@
-import 'package:mynotes/views/veryify_email_view.dart';
-
-const loginRoute = '/login/';
-const registerRoute = '/register/';
-const notesRoute = '/notes/';
-const verifyEmailRoute = '/verify-email/';
 const createOrUpdateNoteRoute = '/notes/new-note/';
