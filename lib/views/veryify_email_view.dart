2cfaf9cea27f9eb16c988e01ca3d2c08f2737420
vbrunoro <vfmbrunoro@gmail.com>
2023-05-12 12:14:14 -0300

    add localization

--- lib/views/veryify_email_view.dart
+++ lib/views/veryify_email_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mynotes/extensions/buildcontext/loc.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
 
@@ -14,20 +15,27 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(context.loc.verify_email),
+      ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const Text(
-                "We've sent you an email verification. Please open it to verify your account."),
-            const Text(
-                "If you haven't received a verification email yet, press the button below"),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                context.loc.verify_email_view_prompt,
+              ),
+            ),
             TextButton(
-              onPressed: () async {
+              onPressed: () {
                 context.read<AuthBloc>().add(
                       const AuthEventSendEmailVerification(),
                     );
               },
-              child: const Text('Send email verification'),
+              child: Text(
+                context.loc.verify_email_send_email_verification,
+              ),
             ),
             TextButton(
               onPressed: () async {
@@ -35,7 +43,9 @@ class _VerifyEmailViewState extends State<VerifyEmailView> {
                       const AuthEventLogOut(),
                     );
               },
-              child: const Text('Restart'),
+              child: Text(
+                context.loc.restart,
+              ),
             )
           ],
         ),
